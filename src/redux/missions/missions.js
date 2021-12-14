4e4e34f758aa5ffbaf8965ea5f66c6298cd73f5a
Taher <ttaherhagui@gmail.com>
2021-12-14 10:05:28 +0100

    configure mission reducers to manage display mission action.

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,13 +1,60 @@
+const LOAD_MISSION = 'space/missions/LOAD_MISSION';
+const MISSION_START = 'space/missions/MISSION_START';
+const MISSION_FAILED = 'space/missions/MISSION_FAILED';
+const loadMission = (payload) => ({
+  type: LOAD_MISSION,
+  payload,
+});
+
+const loadingStart = () => ({
+  type: MISSION_START,
+});
+
+const manageFailure = (payload) => ({
+  type: MISSION_FAILED,
+  payload,
+});
 const fetchData = () => fetch('https://api.spacexdata.com/v3/missions');
-fetchData(); // just created to pass linter check
 
-const initialState = [];
+const selectData = (data) => {
+  const { mission_id: id, mission_name: name, description } = data;
+  return {
+    id,
+    name,
+    description,
+  };
+};
+export const loadMissionsData = (dispatch) => {
+  dispatch(loadingStart());
+  fetchData()
+    .then((res) => res.json())
+    .then((result) => dispatch(loadMission(result)))
+    .catch((err) => dispatch(manageFailure(err.message)));
+};
 
-const reducer = (state = initialState, action) => {
+const missionsReducer = (state = { missions: [] }, action) => {
   switch (action.type) {
+    case MISSION_START:
+      return {
+        ...state,
+        loading: true,
+      };
+    case MISSION_FAILED:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    case LOAD_MISSION:
+      return {
+        ...state,
+        missions: action.payload.filter(selectData),
+        loading: false,
+        error: null,
+      };
     default:
       return state;
   }
 };
 
-export default reducer;
+export default missionsReducer;
