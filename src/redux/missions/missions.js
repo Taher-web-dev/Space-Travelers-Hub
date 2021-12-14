fa682d31a930f95beacd615e4ca2e2a1ce9527ff
Taher <ttaherhagui@gmail.com>
2021-12-14 09:07:57 +0100

    create fetch function.

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,3 +1,6 @@
+const fetchData = () => fetch('https://api.spacexdata.com/v3/missions');
+fetchData(); // just created to pass linter check
+
 const initialState = [];
 
 const reducer = (state = initialState, action) => {
